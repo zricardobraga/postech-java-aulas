b4ebb5d2f0c9a77bfda1629e34bd2c82b24dd420
zricardobraga <zericardobraga@gmail.com>
2024-11-11 20:43:20 -0300

    :tada: - create project

---
+++ aula-02/pet-tech/pet-tech/src/test/java/br/com/fiap/pet_tech/pet_tech/PetTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pet_tech.pet_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
